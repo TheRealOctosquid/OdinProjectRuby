0c55424075d7e03bcbeb301506ce5feaa1765ded
Alexander Cordisco <99907232+TheRealOctosquid@users.noreply.github.com>
2024-08-03 10:27:43 -0700

    Update method_practice_2.rb

--- ruby-excercises/method_practice_2.rb
+++ ruby-excercises/method_practice_2.rb
@@ -8,13 +8,18 @@ end
 
 #solution
 
-def triple_adder(a, b, c)
+def triple_add(a, b, c)
    x = a.to_s + b.to_s + c.to_s
    "#{x}!"
 end   
 
+puts triple_add(1,2,5) # "125!"
 
-# need to convert agruments to strings because you (usually) can't add different datatypes together., as well as declare a 4th variablle to hold the result of the addition. Finally, wrapping x in quotes, and adding an exclamation mark.
+# I needed to convert agruments to strings because you (usually) can't add different datatypes together, 
+# as well as declare a 4th variablle to hold the result of the addition. 
+
+# Finally, wrapping x in quotes, and adding an exclamation mark. The use of to_s converts all the arguments into strings, 
+# so a user can pass any type of argument in and triple_add won't crash.
 
 #  more condensed version
 

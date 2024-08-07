15e39d68145785b214a9a387b2d3968c5451c926
Alexander Cordisco <99907232+TheRealOctosquid@users.noreply.github.com>
2024-08-06 21:08:51 -0700

    Add files via upload

---
+++ ruby-excercises/array_excercises_completed.rb
@@ -0,0 +1,128 @@
+def nil_array(number)
+  # return an array containing `nil` the given number of times
+  # Answer
+  Array.new(number)
+end
+
+
+
+def first_element(array)
+  # return the first element of the array
+  # Answer
+  array.first
+end
+
+
+
+def third_element(array)
+  # return the third element of the array
+  # Answer
+  array[2]
+end
+
+
+
+def last_three_elements(array)
+  # return the last 3 elements of the array
+  # Answer
+  array.last(3)
+end
+
+
+
+def add_element(array)
+  # add an element (of any value) to the array
+  # Answer
+  # array.push(1)
+  # or
+  array << 1
+end
+
+
+
+
+def remove_last_element(array)
+  # Step 1: remove the last element from the array
+  # Step 2: return the array (because Step 1 returns the value of the element removed)
+  # Answer step 1
+  array.pop
+  # Answer step 2
+  array
+end
+
+
+
+
+def remove_first_three_elements(array)
+  # Step 1: remove the first three elements
+
+  # Step 2: return the array (because Step 1 returns the values of the elements removed)
+  
+  # Answer step 1
+  array.shift(3)
+  # Answer step 2
+  array
+end
+
+
+
+
+def array_concatenation(original, additional)
+  # return an array adding the original and additional array together
+  # Answer
+  origional + additional
+end
+
+
+
+def array_difference(original, comparison)
+  # return an array of elements from the original array that are not in the comparison array
+  # Answer
+  orginal - comparison
+end
+
+
+def empty_array?(array)
+  # return true if the array is empty
+  # Answer 
+  array.empty?
+end
+
+
+
+def reverse(array)
+  # return the reverse of the array
+  # Answer  
+  array.reverse
+end
+
+
+
+def array_length(array)
+  # return the length of the array
+  # Answer
+  array_length.length
+end
+
+
+
+def include?(array, value)
+  # return true if the array includes the value
+  # My answer [1,2,3].include?(1)]
+  # Solution
+  array.include?(value)
+end
+
+
+
+def join(array, separator)
+  # return the result of joining the array with the separator
+  # Solution 
+  array.join(separator)
+end
+
+
+# I did not understand this one as well as I would have liked.
+# Simplicity and ease of readability are the two most important things.
+# Most of these were straighforward, a few were a bit more challenging, 
+# but all of them were simple in their solution. 

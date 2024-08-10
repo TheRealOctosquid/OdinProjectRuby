ae3697c4cdbe23e11ae28cdff85893acb8b62cac
Alexander Cordisco <99907232+TheRealOctosquid@users.noreply.github.com>
2024-08-10 06:36:37 -0700

    Add files via upload

---
+++ ruby-excercises/method_explanation.rb
@@ -0,0 +1,13 @@
+# Creating a Method
+# def tells Ruby that this is the start of a Method definition
+# say_hello is the name of the Method
+# "Hello World" is the code that gets executed when the Method is called
+# end tells Ruby that this is the end of the Method definition
+# To call the method you can simply type the name of the Method
+
+def say_hello
+  "Hello World"
+end
+
+puts say_hello
+

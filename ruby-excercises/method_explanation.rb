9e717c20f002f077d7fbeb50d20bc84badcaca15
Alexander Cordisco <99907232+TheRealOctosquid@users.noreply.github.com>
2024-08-10 06:37:11 -0700

    Update method_explanation.rb

--- ruby-excercises/method_explanation.rb
+++ ruby-excercises/method_explanation.rb
@@ -11,3 +11,21 @@ end
 
 puts say_hello
 
+# Best Practices
+# Methods should be defined at the top of the file
+# Methods should be defined before calling them
+# Methods should start with a lowercase as Ruby is case sensitive
+# Names that start with a capital are constants 
+# Methods cannot be one of the Ruby keywords such as end, while, or for. 
+# Cannot use certain symbols other than _, ?, !, and =. 
+# Cannot use ?, !, or = anywhere other than the end of a method name.
+# Cannot begin a method name with a number.
+
+method_name      # valid
+_name_of_method  # valid
+1_method_name    # invalid
+method_27        # valid
+method?_name     # invalid
+method_name!     # valid
+begin            # invalid (Ruby reserved word)
+begin_count      # valid

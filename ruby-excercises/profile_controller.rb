467072c91901035e0565ac2fb2c3e12986ebb423
Alexander Cordisco <99907232+TheRealOctosquid@users.noreply.github.com>
2024-08-12 07:07:45 -0700

    Delete ruby-excercises/profile_controller.rb

--- ruby-excercises/profile_controller.rb
+++
@@ -1,8 +0,0 @@
-class ProfileController < ActionController::Base
-
-  # Danger: since we are not specifying which parameters should be editable,
-  # an attack can update any values on the their profile!
-  def update_username
-    current_user.update!(params)
-  end
-end
